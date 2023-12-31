6d3ec434116390c8857e0c1c92fd3eacbe61d098
Himanshu Rana <himanshurjj@gmail.com>
2023-12-31 21:13:17 +0530

    deleted dist folder

--- dist/src/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const trie_node_1 = require("./trie-node");
-const root = new trie_node_1.TrieNode();
-root.insert('hello');
-root.insert('hell');
-console.log(root.search('hell'));
-console.log(root.search('hello'));
-console.log(root.search('a'));
--- dist/src/utility.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getIndexOfCharacter = void 0;
-const getIndexOfCharacter = (character) => {
-    return character.charCodeAt(0) - 'a'.charCodeAt(0);
-};
-exports.getIndexOfCharacter = getIndexOfCharacter;
